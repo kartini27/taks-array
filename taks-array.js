804970ceeaabbd3936826901b90c133771cc0412
kartini <126692640+kartini27@users.noreply.github.com>
2023-03-08 22:06:47 -0800

    Add files via upload

---
+++ taks-array.js
@@ -0,0 +1,44 @@
+//push array 
+let buku1 ='bahasa indonesia';
+let buku2 ='matematika';
+let buku3 ='bahasa inggris';
+let buku4 = 'ipa';
+let buku5 ='ips';
+let buku = ['bahasa indonesia', 'matematika', 'bahasa inggris','ipa', 'ips'];
+console.log('push array');
+buku.push('fisika', 'kimia',' biologi','penjaskes','ekonomi', 'akutansi');
+console.log(buku)
+
+// index array
+let wadah1 = 'saos';
+let wadah2 = 'kecap manis';
+let wadah3 = 'kecap asin';
+let wadah4 = 'susu bubuk';
+let wadah5 = 'susu cair';
+let wadah6 = 'creamer';
+let wadah = ['saos', 'kecap manis', 'kecap asin', 'susu bubuk', 'susu cair', 'creamer'];
+console.log (wadah[0]); 
+console.log (wadah[1]);
+console.log (wadah[2]);
+console.log (wadah[3]);
+console.log (wadah[4]);
+console.log (wadah[5]);
+console.log (wadah)
+
+// ganti data array
+console.log('ganti data array');
+let nama1 = 'kartini';
+let nama2 = 'soimah';
+let nama3 = 'angga';
+let nama4 = 'fernando';
+let nama5 = 'richard';
+let nama6 = 'rachel';
+let nama = ['kartini', 'soimah', 'angga', 'fernando','richard', 'rachel'];
+console.log('ganti data array');
+nama[1] = 'fani'
+nama[3] = 'saka'
+nama[4] = 'reza'
+nama[5] = 'alfandi'
+console.log(nama)
+
+
